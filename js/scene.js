1e855627647ed159d23faae4011acc321645c087
Chao Zheng <chaoszh@foxmail.com>
2020-06-10 20:13:46 +0800

    temp

---
+++ js/scene.js
@@ -0,0 +1,10 @@
+//游戏设置
+import { init } from './game/index.js'
+//样式设置
+import '../css/style.less'
+//引入FontAwesome
+const FontAwesome = require('font-awesome/less/font-awesome.less')
+//引入audio.js
+const Audio = require('audio')
+
+init()
